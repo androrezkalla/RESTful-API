36ccade69c1040bff3c03ed971c45ab641543518
Andro Rezkalla <78698964+androrezkalla@users.noreply.github.com>
2023-07-06 15:35:26 -0400

    implemented error handling

--- rest-api/server.js
+++ rest-api/server.js
@@ -1,66 +1,82 @@
 const express = require("express");
 const app = express();
-app.use(express.json())
+app.use(express.json());
 const port = 4000;
 
 // Sample resource populated by cars
 const cars = [
-    { id: 1, brand: 'Toyota', model: '86', year: 2020 },
-    { id: 2, brand: 'Subaru', model: 'BRZ', year: 2019 },
-    { id: 3, brand: 'Scion', model: 'FRS', year: 2021 }
-]; 
+  { id: 1, brand: 'Toyota', model: '86', year: 2020 },
+  { id: 2, brand: 'Subaru', model: 'BRZ', year: 2019 },
+  { id: 3, brand: 'Scion', model: 'FRS', year: 2021 }
+];
 
 app.get("/", (req, res) => {
   res.send("Welcome to Andro's custom API!");
 });
 
-//List all cars in the resources
+// List all cars in the resources
 app.get('/cars', (req, res) => {
-    res.json(cars);
+  res.json(cars);
 });
 
-//Get car by ID
+// Get car by ID
 app.get("/cars/:id", (req, res) => {
-    const carId = parseInt(req.params.id, 10);
-    const car = cars.find((car) => car.id === carId);
+  const carId = parseInt(req.params.id, 10);
+  const car = cars.find((car) => car.id === carId);
+  if (!car) {
+    res.status(404).send({ error: "Car not found" });
+  } else {
     res.send(car);
+  }
 });
 
 // Create a new car post
 function getNextIdFromCollection(collection) {
-    if (collection.length === 0) return 1;
-    const lastRecord = collection[collection.length - 1];
-    return lastRecord.id + 1;
-  }
-  app.post("/cars", (req, res) => {
-    const newCar = {
-      ...req.body,
-      id: getNextIdFromCollection(cars)
-    };
-    console.log("newCar", newCar);
-    cars.push(newCar);
-    res.send(newCar);
+  if (collection.length === 0) return 1;
+  const lastRecord = collection[collection.length - 1];
+  return lastRecord.id + 1;
+}
+app.post("/cars", (req, res) => {
+  const newCar = {
+    ...req.body,
+    id: getNextIdFromCollection(cars)
+  };
+  console.log("newCar", newCar);
+  cars.push(newCar);
+  res.send(newCar);
 });
-  
+
 // Update a specific car
 app.patch("/cars/:id", (req, res) => {
-    const carId = parseInt(req.params.id, 10);
-    const carUpdates = req.body;
-    const carIndex = cars.findIndex(car => car.id === carId);
+  const carId = parseInt(req.params.id, 10);
+  const carUpdates = req.body;
+  const carIndex = cars.findIndex(car => car.id === carId);
+  if (carIndex === -1) {
+    res.status(404).send({ error: "Car not found" });
+  } else {
     const updatedCar = { ...cars[carIndex], ...carUpdates };
     cars[carIndex] = updatedCar;
     console.log("updatedCar", updatedCar);
     res.send(updatedCar);
+  }
 });
 
-// Delete a specefic car
+// Delete a specific car
 app.delete("/cars/:id", (req, res) => {
-    const carId = parseInt(req.params.id, 10);
-    const carIndex = cars.findIndex(car => car.id === carId);
+  const carId = parseInt(req.params.id, 10);
+  const carIndex = cars.findIndex(car => car.id === carId);
+  if (carIndex === -1) {
+    res.status(404).send({ error: "Car not found" });
+  } else {
     cars.splice(carIndex, 1);
     res.send({ message: "Car deleted successfully" });
+  }
 });
 
+// Error handling for invalid routes
+app.use((req, res) => {
+  res.status(404).send({ error: "Invalid route" });
+});
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
